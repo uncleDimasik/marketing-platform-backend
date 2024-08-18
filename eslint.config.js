7ddb6e9d27d323aab3838d2fa8663a019377ed1b
Dmytro Davydenko <dimasik1204dim@gmail.com>
2024-08-18 18:14:56 +0300

    fix:eslint

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+const globals = require('globals');
+const {configs} = require('@eslint/js');
+
+module.exports = [
+    {
+        files: ['**/*.js'],
+        ignores: ['**/*.config.js'],
+
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: 'latest',
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                console: 'readonly',
+            },
+        },
+
+        rules: {
+  
+            ...configs.recommended.rules,
+
+            'no-console': 'warn',
+            eqeqeq: 'error',
+            curly: 'error',
+            'no-var': 'error',
+            'no-unused-vars': ['warn', {vars: 'all', args: 'after-used', ignoreRestSiblings: false}],
+        },
+    },
+];
