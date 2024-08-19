698e8aac735c25a4c31b3348cd0a0adadb5b31a0
Dmytro Davydenko <dimasik1204dim@gmail.com>
2024-08-19 06:36:21 +0300

    fix: cookie

--- utils/cookieUtils.js
+++ utils/cookieUtils.js
@@ -1,24 +1,25 @@
-const getCookieOptions = ({ clear = false } = {}) => {
-  return {
-    maxAge: clear ? 0 : 30 * 24 * 60 * 60 * 1000, //30d
-    httpOnly: true,
-    signed: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'Lax',
-    path: '/',
-  };
+const getCookieOptions = ({clear = false} = {}) => {
+    return {
+        maxAge: clear ? 0 : 30 * 24 * 60 * 60 * 1000, //30d
+        httpOnly: true,
+        signed: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none',
+        path: '/',
+
+    };
 };
 
 const setRefreshTokenCookie = (res, token) => {
-  console.log(process.env.COOKIE_DOMAIN);
-  res.cookie('refreshToken', token, getCookieOptions());
+    console.log(process.env.COOKIE_DOMAIN);
+    res.cookie('refreshToken', token, getCookieOptions());
 };
 
 const clearRefreshTokenCookie = (res) => {
-  res.clearCookie('refreshToken', getCookieOptions({ clear: true }));
+    res.clearCookie('refreshToken', getCookieOptions({clear: true}));
 };
 
 module.exports = {
-  setRefreshTokenCookie,
-  clearRefreshTokenCookie,
+    setRefreshTokenCookie,
+    clearRefreshTokenCookie,
 };
