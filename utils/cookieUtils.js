a011acf1c9b3b759b13af2fcdecc15027323d689
Dmytro Davydenko <dimasik1204dim@gmail.com>
2024-08-19 07:33:06 +0300

    fix:cookie

--- utils/cookieUtils.js
+++ utils/cookieUtils.js
@@ -5,7 +5,6 @@ const getCookieOptions = ({ clear = false } = {}) => {
     signed: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.COOKIE_SAME_SITE,
-    path: process.env.COOKIE_PATH || '/',
     domain: process.env.COOKIE_DOMAIN || undefined,
   };
 };
